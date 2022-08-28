fab6f1df505ab6ccc4632b8694ce40e0d988199e
aysha12345 <91528182+aysha12345@users.noreply.github.com>
2022-08-28 11:07:19 +0100

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        
+        int originalNumber = x;
+        int reverse = 0;
+        int remainder = 0;
+        
+        while(x > 0){
+            remainder = x % 10;
+            reverse = (reverse * 10) + remainder;
+            x = x / 10;
+        }
+        
+        if(reverse == originalNumber){
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+}
