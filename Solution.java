6573c8dcad50986af0f92da5865de0a3bd578648
aysha12345 <91528182+aysha12345@users.noreply.github.com>
2022-08-28 11:08:50 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -11,7 +11,7 @@ class Solution {
         while(x > 0){
             remainder = x % 10;
             reverse = (reverse * 10) + remainder;
-            x = x / 10;
+            x = x // 10;
         }
         
         if(reverse == originalNumber){
